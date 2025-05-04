bea320a4518bac0419f58e9240735050be56c2cb
Dahiru Aliyu Adamu <dahirualiyuadamu@gmail.com>
2025-05-04 16:07:40 +0100

    fix Monog URI

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express()
 dotenv.config()
 
 const PORT = process.env.PORT || 7000
-const MONGOURL = process.env.MONGO_URI
+const MONGOURI = process.env.MONGO_URI
 
 app.use(express.json()) // for using json data
 app.use(express.urlencoded({ extended: true })) // for using x-www-form-urlencoded
